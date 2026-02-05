f90f8e21d3b37316be9816f03f474b8b89e85b94
ratheeshtvla <ratheeshtiruvalla@gmail.com>
2026-02-05 09:03:43 +0530

    Add files via upload

---
+++ 2_nav.php
@@ -0,0 +1,327 @@
+	<a class="usa-skipnav" href="#main-content">Skip to main content</a>
+	
+	<section
+		class="usa-banner"
+		aria-label="Official website of the United States government">
+		<div class="grid-container-widescreen">
+		<div class="usa-accordion">
+
+			<header class="usa-banner__header">
+				<div class="usa-banner__inner grid-container-widescreen">
+					<div class="grid-col-auto">
+						<img
+							aria-hidden="true"
+							class="usa-banner__header-flag"
+							src="uswds/dist/img/us_flag_small.png"
+							alt="" />
+					</div>
+					<div class="grid-col-fill tablet:grid-col-auto" aria-hidden="true">
+						<p class="usa-banner__header-text">
+							An U.S. Web Design System (USWDS) template.
+						</p>
+						<p class="usa-banner__header-action">Here’s how you know</p>
+					</div>
+					<button
+						type="button"
+						class="usa-accordion__button usa-banner__button"
+						aria-expanded="false"
+						aria-controls="gov-banner-default">
+						<span class="usa-banner__button-text">Here’s how you know</span>
+					</button>
+				</div>
+			</header>
+
+			<div
+				class="usa-banner__content usa-accordion__content"
+				id="gov-banner-default">
+				<div class="grid-row grid-gap-lg">
+					<div class="usa-banner__guidance tablet:grid-col-6">
+						<img
+							class="usa-banner__icon usa-media-block__img"
+							src="uswds/dist/img/icon-dot-gov.svg"
+							role="img"
+							alt=""
+							aria-hidden="true" />
+						<div class="usa-media-block__body">
+							<p>
+								<strong>Official websites use .gov</strong><br />A
+								<strong>.gov</strong> website belongs to an official government
+								organization in the United States.
+							</p>
+						</div>
+					</div>
+					<div class="usa-banner__guidance tablet:grid-col-6">
+						<img
+							class="usa-banner__icon usa-media-block__img"
+							src="uswds/dist/img/icon-https.svg"
+							role="img"
+							alt=""
+							aria-hidden="true" />
+						<div class="usa-media-block__body">
+							<p>
+								<strong>Secure .gov websites use HTTPS</strong><br />A
+								<strong>lock</strong> (
+								<span class="icon-lock"><svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="52"
+										height="64"
+										viewBox="0 0 52 64"
+										class="usa-banner__lock-image"
+										role="img"
+										aria-labelledby="banner-lock-description"
+										focusable="false">
+										<title id="banner-lock-title">Lock</title>
+										<desc id="banner-lock-description">
+											Locked padlock icon
+										</desc>
+										<path
+											fill="#000000"
+											fill-rule="evenodd"
+											d="M26 0c10.493 0 19 8.507 19 19v9h3a4 4 0 0 1 4 4v28a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V32a4 4 0 0 1 4-4h3v-9C7 8.507 15.507 0 26 0zm0 8c-5.979 0-10.843 4.77-10.996 10.712L15 19v9h22v-9c0-6.075-4.925-11-11-11z" />
+									</svg> </span>) or <strong>https://</strong> means you’ve safely connected to
+								the .gov website. Share sensitive information only on official,
+								secure websites.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>		
+	</section>
+
+	<div class="usa-overlay"></div>
+<header class="usa-header usa-header--basic ">
+  <div class="usa-nav-container grid-container-widescreen">
+ <div class="usa-navbar">
+			<div class="usa-logo">
+				<em class="usa-logo__text"><a href="./" title="&lt;Project title&gt;">RFPMart City</a></em>
+			</div>
+			<button class="usa-menu-btn" aria-label="Open menu">
+  <svg class="usa-icon" aria-hidden="true">
+    <use xlink:href="uswds/dist/img/sprite.svg#menu"></use>
+  </svg>
+</button>
+
+		</div>
+
+    <nav aria-label="Primary navigation" class="usa-nav">
+      <button type="button" class="usa-nav__close">
+        <img src="images/close.svg" role="img" alt="Close" />
+      </button>
+      <ul class="usa-nav__primary usa-accordion">
+		<li class="usa-nav__primary-item">
+			<a href="https://www.rfpmart.com" class="usa-nav-link">Home</a>
+		</li>
+		
+<li class="usa-nav__primary-item">
+						<button type="button" class="usa-accordion__button usa-nav__link usa-nav__link" aria-expanded="false" aria-controls="extended-mega-nav-section-one">
+							<span>Departments</span>
+						</button>
+						<div id="extended-mega-nav-section-one" class="usa-nav__submenu usa-megamenu" hidden="">
+							<div class="grid-row grid-gap-4">
+
+								<div class="usa-col">
+									<ul class="usa-nav__submenu-list">
+										<li class="usa-nav__submenu-item">
+											<a href="departments-city-council.html">City Council</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-mayors-office.html">Mayor's Office</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-roads-transportation.html">Roads &amp; Transportation</a>
+										</li>
+									</ul>
+								</div>
+
+								<div class="usa-col">
+									<ul class="usa-nav__submenu-list">
+										<li class="usa-nav__submenu-item">
+											<a href="departments-procurements-1.html">Procurements - 1</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-procurements-2.html">Procurements - 2</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-procurements-listing.html">Procurements Listing</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-procurements-contracts.html">Procurement Contracts</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-procurements-detail.html">Procurement Detail</a>
+										</li>
+									</ul>
+								</div>
+
+								<div class="usa-col">
+									<ul class="usa-nav__submenu-list">
+										<li class="usa-nav__submenu-item">
+											<a href="departments-public-safety.html">Public Safety</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-police.html">Police</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-fire.html">Fire &amp; Rescue</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-emergency-management.html">Emergency Management</a>
+										</li>
+									</ul>
+								</div>
+
+								<div class="usa-col">
+									<ul class="usa-nav__submenu-list">
+										<li class="usa-nav__submenu-item">
+											<a href="departments-garbage-recycling.html">Garbage &amp; Recycling</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-garbage-collection.html">Garbage Collection</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-hazardous-waste.html">Hazardous Waste</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-recycling.html">Recycling</a>
+										</li>
+									</ul>
+								</div>
+
+								<div class="usa-col">
+									<ul class="usa-nav__submenu-list">
+										<li class="usa-nav__submenu-item">
+											<a href="departments-directory-accordion.html">Directory Accordion</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-directory-button.html">Directory Button</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-directory-card.html">Directory Card</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="usa-nav__primary-item">
+						<button type="button" class="usa-accordion__button usa-nav__link usa-nav__link" aria-expanded="false" aria-controls="extended-mega-nav-section-one">
+							<span>Services</span>
+						</button>
+						<div id="extended-mega-nav-section-one" class="usa-nav__submenu usa-megamenu" hidden="">
+							<div class="grid-row grid-gap-4">
+
+								<div class="usa-col">
+									<ul class="usa-nav__submenu-list">
+										<li class="usa-nav__submenu-item">
+											<a href="departments-city-council.html">City Council</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-mayors-office.html">Mayor's Office</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-roads-transportation.html">Roads &amp; Transportation</a>
+										</li>
+									</ul>
+								</div>
+
+								<div class="usa-col">
+									<ul class="usa-nav__submenu-list">
+										<li class="usa-nav__submenu-item">
+											<a href="departments-procurements-1.html">Procurements - 1</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-procurements-2.html">Procurements - 2</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-procurements-listing.html">Procurements Listing</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-procurements-contracts.html">Procurement Contracts</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-procurements-detail.html">Procurement Detail</a>
+										</li>
+									</ul>
+								</div>
+
+								<div class="usa-col">
+									<ul class="usa-nav__submenu-list">
+										<li class="usa-nav__submenu-item">
+											<a href="departments-public-safety.html">Public Safety</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-police.html">Police</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-fire.html">Fire &amp; Rescue</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-emergency-management.html">Emergency Management</a>
+										</li>
+									</ul>
+								</div>
+
+								<div class="usa-col">
+									<ul class="usa-nav__submenu-list">
+										<li class="usa-nav__submenu-item">
+											<a href="departments-garbage-recycling.html">Garbage &amp; Recycling</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-garbage-collection.html">Garbage Collection</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-hazardous-waste.html">Hazardous Waste</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-recycling.html">Recycling</a>
+										</li>
+									</ul>
+								</div>
+
+								<div class="usa-col">
+									<ul class="usa-nav__submenu-list">
+										<li class="usa-nav__submenu-item">
+											<a href="departments-directory-accordion.html">Directory Accordion</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-directory-button.html">Directory Button</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="departments-directory-card.html">Directory Card</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="usa-nav__primary-item">
+			<a href="https://www.rfpmart.com" class="usa-nav-link">About</a>
+		</li>
+      <li class="usa-nav__primary-item">
+						<button type="button" class="usa-accordion__button usa-nav__link" aria-expanded="false" aria-controls="basic-nav-section-four">
+							<span>Contact</span>
+						</button>
+						<div id="basic-nav-section-four" class="usa-nav__submenu" hidden="">
+							<div class="grid-row grid-gap-4">
+								<div class="usa-col">
+									<ul class="usa-nav__submenu-list">
+										<li class="usa-nav__submenu-item">
+											<a href="sample-pages-contact.html">Contact Us</a>
+										</li>
+										<li class="usa-nav__submenu-item">
+											<a href="sample-pages-contact-form.html">Contact Us Form</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</li>
+				
+      </ul>
+
+    </nav>
+  </div>
+
+</header>
